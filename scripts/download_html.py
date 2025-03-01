fdb950c1b869af3b7194eef9cefc88e90c00ee72
Dominik Serafin <serafin652002@gmail.com>
2025-03-01 16:33:31 +0100

    poprawki

--- scripts/download_html.py
+++ scripts/download_html.py
@@ -1,9 +1,4 @@
-import os
 import requests
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
-DATA_DIR = os.path.join(BASE_DIR, "data/html")
-os.makedirs(DATA_DIR, exist_ok=True)
 
 def fetch_html(url, filename=None, save=True):
 
@@ -13,12 +8,4 @@ def fetch_html(url, filename=None, save=True):
     if response.status_code != 200:
         raise Exception(f"Error downloading html: {response.status_code}")
 
-    html_content = response.text
-
-    if save and filename:
-        filepath = os.path.join(DATA_DIR, filename)
-        with open(filepath, "w", encoding="utf-8") as f:
-            f.write(html_content)
-        print(f"'{filename}' saved in {filepath}")
-
-    return html_content
+    return response.text
--- scripts/groups_scraper.py
+++ scripts/groups_scraper.py
@@ -1,31 +1,27 @@
 import os
 from bs4 import BeautifulSoup
-from download_html import fetch_html
+from download_html import fetch_html 
 
 GROUPS_URL = "https://planzajec.wcy.wat.edu.pl/pl/rozklad"
-
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data")
-HTML_DIR = os.path.join(DATA_DIR, "html")
-FILENAME = os.path.join(HTML_DIR, "groups.html")
 OUTPUT_FILE = os.path.join(DATA_DIR, "groups.txt")
 
-def parse_groups(html_file, output_file):
-
-    with open(html_file, "r", encoding="utf-8") as f:
-        soup = BeautifulSoup(f, "html.parser")
+def parse_groups(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
 
     groups = [option.text.strip() for option in soup.find_all("option") if option.text.strip()]
-
-    if groups and "- Wybierz grupę -" in groups[0]:
-        groups.pop(0)
-
+    
+    groups = [group for group in groups if "- Wybierz grupę -" not in group]
     groups = [group.rstrip(".") for group in groups]
 
-    with open(output_file, "w", encoding="utf-8") as f:
-        f.write("\n".join(groups))
-
-    print(f"Groups saved in: '{output_file}")
+    return groups
 
 if __name__ == "__main__":
-    fetch_html(GROUPS_URL, FILENAME)
-    parse_groups(FILENAME, OUTPUT_FILE)
+    html_content = fetch_html(GROUPS_URL) 
+    groups = parse_groups(html_content)
+
+    os.makedirs(DATA_DIR, exist_ok=True)
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+        f.write("\n".join(groups))
+
+    print(f"Groups saved in: {OUTPUT_FILE}")
