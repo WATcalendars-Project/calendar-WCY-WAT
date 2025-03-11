42b772ff66044fe523ca594916b15b32a0a881b0
Dominik Serafin <serafin652002@gmail.com>
2025-03-11 21:59:50 +0100

    dodanie httpx przez zmiane certyfikatow strony

--- scripts/download_html.py
+++ scripts/download_html.py
@@ -1,11 +1,19 @@
-import requests
+import httpx
 
-def fetch_html(url, filename=None, save=True):
+def fetch_html(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    try:
+        with httpx.Client(verify=False, timeout=20) as client:  # 20 sekund timeoutu
+            response = client.get(url)
+            response.raise_for_status()  # Wymusi zgłoszenie błędu HTTP, jeśli wystąpi
+            return response.text
+    except httpx.HTTPStatusError as e:
+        print(f"Error: HTTPStatusError: {e.response.status_code} - {e.response.text}")
+    except httpx.TimeoutException:
+        print("Error: TimeoutException")
+    except httpx.RequestError as e:
+        print(f"Error: RequestError: {e}")
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"}
-
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        raise Exception(f"Error downloading html: {response.status_code}")
-
-    return response.text
+    return None
